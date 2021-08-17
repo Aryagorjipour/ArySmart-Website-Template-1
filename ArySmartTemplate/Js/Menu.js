cea3be817312858d7f81b020774f512c5ea45bbd
Aryagorjipour <arygorji.pour@gmail.com>
2021-08-17 21:09:24 +0430

    Uploaded the entire project

---
+++ ArySmartTemplate/Js/Menu.js
@@ -0,0 +1,25 @@
+// 
+//     <div id="menuBar" class="row fixed-top mnuBG1" style="margin: 0px 0px 0px 0px !important;">
+//         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+//             <nav>
+//                 <ul>
+//                 </ul>
+//             </nav>
+//         </div>
+//     </div> 
+
+
+//      <script>
+//         $(document).ready(function () {
+//             $(window).on('scroll', function (event) {
+//                 var scrollValue = $(window).scrollTop();
+//                 if (scrollValue > 10) {
+//                     $('#menuBar').removeClass('mnuBG1');
+//                     $('#menuBar').addClass('mnuBG2');
+//                 } else {
+//                     $('#menuBar').removeClass('mnuBG2');
+//                     $('#menuBar').addClass('mnuBG1');
+//                 }
+//             });
+//         });
+//     </script>
